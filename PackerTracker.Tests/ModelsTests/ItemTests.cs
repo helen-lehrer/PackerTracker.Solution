b66a8f9e7e50a82135200f609c53bbdda22eaab3
Helen Lehrer <hlehrer@outlook.com>
2022-09-28 12:04:40 -0700

    Pass 4th test and add Dispose
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- PackerTracker.Tests/ModelsTests/ItemTests.cs
+++ PackerTracker.Tests/ModelsTests/ItemTests.cs
@@ -5,35 +5,70 @@ using System;
 
 namespace PackerTracker.Tests
 {
+ 
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+    public Item itemOne;
+    public string name;
+    public int weight;
+    public int price;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+      name = "Osprey Hiking Backpack";
+      weight = 5;
+      price = 150;
+      itemOne = new Item(name, weight, price);
+    }   
+
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      //Arrange
-      string name = "Osprey Hiking Backpack";
-      int weight = 5;
-      int price = 150;
-
-      Item newItem = new Item(name, weight, price);
-      Assert.AreEqual(typeof(Item), newItem.GetType());
+      Assert.AreEqual(typeof(Item), itemOne.GetType());
     }
 
     [TestMethod]
     public void GetName_ReturnName_String()
     {
-      //Arrange
-      string name = "Osprey Hiking Backpack";
-      int weight = 5;
-      int price = 150;
-
-      //Act
-      Item newItem = new Item(name, weight, price);
-      string result = newItem.Name;
-
-      //Assert
+      string result = itemOne.Name;
       Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnList_List()
+    {
+      string nameTwo = "REI Camping Stove";
+      int weightTwo = 2;
+      int priceTwo = 15;
+      Item itemTwo = new Item(nameTwo, weightTwo, priceTwo);
+      List<Item> newList = new List<Item> { itemOne, itemTwo };
+
+      List<Item> result = Item.GetAll();
+      Console.WriteLine(itemOne.Name);
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void ClearAll_ReturnEmptyList_List()
+    {
+      itemOne = new Item(name, weight, price);
+      
+      string nameTwo = "REI Camping Stove";
+      int weightTwo = 2;
+      int priceTwo = 15;
+      Item itemTwo = new Item(nameTwo, weightTwo, priceTwo);
+      List<Item> newList = new List<Item> {};
+      Item.ClearAll();
+
+      CollectionAssert.AreEqual(Item.GetAll(), newList);
     }
   }
 }
--- PackerTracker/Models/Item.cs
+++ PackerTracker/Models/Item.cs
@@ -14,10 +14,26 @@ namespace PackerTracker.Models
     public Item(string name, int weight, int price)
     {
       Name = name;
-      Weight= weight;
+      Weight = weight;
       Price = price;
       _instances.Add(this);
       Id = _instances.Count;
     }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    // public static Item Find(int searchId)
+    // {
+    //   return _instances[searchId-1];
+    // }
+
   }
 }
